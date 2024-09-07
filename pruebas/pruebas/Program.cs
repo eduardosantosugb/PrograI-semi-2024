e0c004fa96207d5c21956c7cb0317c0fdf915da2
eduardosantosugb <7edwardc7@gmail.com>
2024-09-06 19:57:43 -0600

    tarea de notas

--- pruebas/pruebas/Program.cs
+++ pruebas/pruebas/Program.cs
@@ -7,27 +7,47 @@ using System.Threading.Tasks;
 
 namespace miPrimerProyecto
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Num1: ");
-            int num1 = int.Parse(Console.ReadLine());
-            sbyte num1 = sbyte.Parse(Console.ReadLine());
+   Console.Write("Lab1: ");
+double lab1 = double.Parse(Console.ReadLine()); //8
+    Console.Write("Lab2: ");
+double lab2 = double.Parse(Console.ReadLine()); //9
+    Console.Write("Parcial 1: ");
+double parcial1 = double.Parse(Console.ReadLine()); //7
+    double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+    Console.WriteLine("La nota de C1 es: {0}", c1);
 
-            Console.Write("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
-            Console.Write("Num2: ");
-            sbyte num2 = sbyte.Parse(Console.ReadLine());
 
-            int respuesta = num1 + num2;
-            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
-            int suma = num1 + num2;
 
-            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, suma);
+Console.Write("Ciclo 2   ");
 
-            //Pausa.
-            Console.ReadLine();
-        }
-    }
+Console.Write("Lab1: ");
+lab1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Lab2: ");
+lab2 = double.Parse(Console.ReadLine());
+
+    Console.Write("Parcial 1: ");
+parcial1 = double.Parse(Console.ReadLine());
+
+    double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+    Console.WriteLine("La nota de C2 es: {0}", c2);
+
+
+Console.Write("Ciclo 3   ");
+
+Console.Write("Lab1: ");
+lab1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Lab2: ");
+lab2 = double.Parse(Console.ReadLine());
+
+    Console.Write("Parcial 1: ");
+parcial1 = double.Parse(Console.ReadLine());
+
+
+    double c3 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
+    Console.WriteLine("La nota de C3 es {0}", c3);
+
+double notaf = c1 + c2 + c3 / 3;
+    Console.WriteLine(" la nota final de Progra 1 es : {0}", notaf);
 }
