58b539160caf3676b1cbdb04039ac9282e9446e6
eduardosantosugb <7edwardc7@gmail.com>
2024-09-08 11:13:13 -0600

    promedio de serie de numeros

--- pruebas/pruebas/Program.cs
+++ pruebas/pruebas/Program.cs
@@ -5,49 +5,20 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace miPrimerProyecto
+namespace miPrimerProyecto { }
+ internal class Program
 {
-   Console.Write("Lab1: ");
-double lab1 = double.Parse(Console.ReadLine()); //8
-    Console.Write("Lab2: ");
-double lab2 = double.Parse(Console.ReadLine()); //9
-    Console.Write("Parcial 1: ");
-double parcial1 = double.Parse(Console.ReadLine()); //7
-    double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-    Console.WriteLine("La nota de C1 es: {0}", c1);
-
-
-
-Console.Write("Ciclo 2   ");
-
-Console.Write("Lab1: ");
-lab1 = double.Parse(Console.ReadLine());
-
-    Console.Write("Lab2: ");
-lab2 = double.Parse(Console.ReadLine());
-
-    Console.Write("Parcial 1: ");
-parcial1 = double.Parse(Console.ReadLine());
-
-    double c2 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-    Console.WriteLine("La nota de C2 es: {0}", c2);
-
-
-Console.Write("Ciclo 3   ");
-
-Console.Write("Lab1: ");
-lab1 = double.Parse(Console.ReadLine());
-
-    Console.WriteLine("Lab2: ");
-lab2 = double.Parse(Console.ReadLine());
-
-    Console.Write("Parcial 1: ");
-parcial1 = double.Parse(Console.ReadLine());
-
-
-    double c3 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-    Console.WriteLine("La nota de C3 es {0}", c3);
-
-double notaf = c1 + c2 + c3 / 3;
-    Console.WriteLine(" la nota final de Progra 1 es : {0}", notaf);
+    static void Main(string[] args)
+    {
+        int[] serie = new int[] { 5, 4, 6, 8, 9 }; //32
+        int suma = 0;
+        foreach (int num in serie)
+        {
+            suma += num;
+        }
+        decimal prom = suma / serie.Length;
+        Console.WriteLine("La suma es: {0}, el promedio {1}", suma, prom);
+        //Pausa.
+        Console.ReadLine();
+    }
 }
